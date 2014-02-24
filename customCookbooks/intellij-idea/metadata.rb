3aa8e2b672d77b7833b75dc12e3964f5340f844e
Tomasz Bartczak <tomasz.bartczak@pragmatists.pl>
2014-02-24 14:11:29 +0100

    removed vm-related files

--- customCookbooks/intellij-idea/metadata.rb
+++
@@ -1,4 +0,0 @@
-version          "0.0.1"
-
-depends "tar", "0.2.0"
-depends "java"
--- customCookbooks/scala-ide/metadata.rb
+++
@@ -1,7 +0,0 @@
-version          "0.0.1"
-
-depends "tar", "0.2.0"
-depends "ark", "0.5.0"
-depends "scala", "0.0.2"
-
-depends "intellij-idea", "0.0.1"
